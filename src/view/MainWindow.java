2264eccddfa7ecd151511129226cd73534ff4ca4
CloudMagician <luzixv1998@icloud.com>
2019-06-17 10:34:23 +0800

    add cardlayout

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -2,9 +2,26 @@ package view;
 
 import model.Account;
 
+import javax.management.Notification;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow {
+    private JPanel MainWindowPanel;
+    private JPanel ButtonPanel;
+    private JPanel CardPanel;
+    private JButton NotificationButton;
+    private JButton PersonalInfoButton;
+    private JButton CourseListButton;
+    private JButton button4;
+    private JButton button5Button;
+    private JButton button6Button;
+    private JPanel NotificationPanel;
+    private JPanel PersonalInfoPanel;
+    private JPanel CourseListPanel;
+
     public MainWindow(Account user) {
         switch (user.getUserType()) {
             case SYSADMIN:
@@ -23,18 +40,25 @@ public class MainWindow {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        NotificationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NotificationPanel.setVisible(true);
+            }
+        });
+        PersonalInfoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        CourseListButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 
-    private JPanel MainWindowPanel;
-    private JPanel ButtonPanel;
-    private JPanel CardPanel;
-    private JButton NotificationButton;
-    private JButton PersonalInfoButton;
-    private JButton CourseListButton;
-    private JButton button4;
-    private JButton button5Button;
-    private JButton button6Button;
-    private JPanel NotificationPanel;
-    private JPanel PersonalInfoPanel;
-    private JPanel CourseListPanel;
+
 }
