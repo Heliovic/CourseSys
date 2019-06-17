1fef909edfa211ea0aa0b5fade579778430f7cd4
CloudMagician <luzixv1998@icloud.com>
2019-06-17 20:06:48 +0800

    delete

--- src/Application.java
+++
@@ -1,9 +0,0 @@
-import view.HomePageView;
-
-public class Application {
-
-    public static void main(String[] args) {
-        HomePageView homePage = new HomePageView();
-        //homePage
-    }
-}
