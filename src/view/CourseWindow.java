c464b02d8289850013467add40a38f74e2b39d3a
CloudMagician <luzixv1998@icloud.com>
2019-06-26 19:14:38 +0800

    add coursewindow

---
+++ src/view/CourseWindow.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class CourseWindow {
+    private JPanel CourseWindowPanel;
+    private JTextArea textArea1;
+    private JTable table1;
+    private JLabel TitleLabel;
+}
